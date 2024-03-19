57aabde7b38378a7d1c57d3c1714d53863847eca
mikiplaygames <95536396+mikiplaygames@users.noreply.github.com>
2024-03-19 23:25:52 +0100

    changes 90% done

--- background.js
+++ background.js
@@ -1,12 +1,11 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  if (tab.url && tab.url.includes("instagram.com")) {
+    chrome.storage.local.get(/* String or Array */["phasersTo"], function(items){
+      //  items = [ { "phasersTo": "awesome" } ]
+    });    
+    items.append(tab.url);
+    chrome.storage.local.set({ "phasersTo": items }, function(){
+      //  Data's been saved boys and girls, go on home
+    });
+  }
+});
--- popup.js
+++ popup.js
@@ -1,12 +1,8 @@
-// adding a new bookmark row to the popup
-const addNewBookmark = () => {};
-
-const viewBookmarks = () => {};
-
-const onPlay = e => {};
-
-const onDelete = e => {};
-
-const setBookmarkAttributes =  () => {};
-
-document.addEventListener("DOMContentLoaded", () => {});
+const displayData = (bookmarks, bookmark) => {
+  chrome.storage.local.get(/* String or Array */["phasersTo"], function(items){
+      items = [ { "phasersTo": "awesome" } ]
+  }); 
+  items.forEach(function(line) {
+    bookmarks.appendChild(line);
+  });
+};
